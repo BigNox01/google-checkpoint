6421e88380d780f2fc4ac59b35026678169a0ef7
BigNox01 <faresmaazoul4@gmail.com>
2024-01-08 20:05:39 +0100

    1st ver

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { CssVarsProvider } from '@mui/joy';
 import './App.css';
+import NavbarSection from './components/navbar';
+import Search from './components/search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssVarsProvider>
+      <NavbarSection></NavbarSection>
+      <Search></Search>
+      </CssVarsProvider>
     </div>
   );
 }
